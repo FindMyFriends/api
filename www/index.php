a7111144d5a5c7780988e386bc21f3b542e0dfac
Dominik Klapuch <klapuchdominik@gmail.com>
2017-10-08 21:41:50 +0200

    Handled OPTIONS

--- www/index.php
+++ www/index.php
@@ -4,35 +4,62 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Klapuch\Application;
 use Klapuch\Ini;
+use Klapuch\Internal;
 use Klapuch\Log;
+use Klapuch\Output;
 use Klapuch\Routing;
 use Klapuch\Uri;
-
 const CONFIGURATION = __DIR__ . '/../App/Configuration/.config.ini',
 	LOCAL_CONFIGURATION = __DIR__ . '/../App/Configuration/.config.local.ini',
 	LOGS = __DIR__ . '/../log',
 	V1_ROUTES_PATH = __DIR__ . '/../App/Configuration/Routes/v1.json';
-echo (new Application\SuitedPage(
+$source = new Ini\CachedSource(
 	new Ini\CombinedSource(
-		new Ini\ValidSource(CONFIGURATION, new Ini\TypedSource(CONFIGURATION)),
+		new Ini\ValidSource(
+			new \SplFileInfo(CONFIGURATION),
+			new Ini\TypedSource(new \SplFileInfo(CONFIGURATION))
+		),
 		new Ini\MutedSource(
 			new Ini\ValidSource(
-				LOCAL_CONFIGURATION,
-				new Ini\TypedSource(LOCAL_CONFIGURATION)
+				new \SplFileInfo(LOCAL_CONFIGURATION),
+				new Ini\TypedSource(new \SplFileInfo(LOCAL_CONFIGURATION))
+			)
+		)
+	)
+);
+
+echo (new class(
+	new Application\SuitedPage(
+		$source,
+		new Log\FilesystemLogs(new Log\DynamicLocation(new Log\DirectoryLocation(LOGS))),
+		new Routing\HttpRoutes(
+			json_decode(file_get_contents(V1_ROUTES_PATH), true),
+			$_SERVER['REQUEST_METHOD']
+		),
+		new Uri\CachedUri(
+			new Uri\BaseUrl(
+				$_SERVER['SCRIPT_NAME'],
+				$_SERVER['REQUEST_URI'],
+				$_SERVER['SERVER_NAME'],
+				$_SERVER['HTTPS'] ?? 'http'
 			)
 		)
 	),
-	new Log\FilesystemLogs(new Log\DynamicLocation(new Log\DirectoryLocation(LOGS))),
-	new Routing\HttpRoutes(
-		json_decode(file_get_contents(V1_ROUTES_PATH), true),
-		$_SERVER['REQUEST_METHOD']
-	),
-	new Uri\CachedUri(
-		new Uri\BaseUrl(
-			$_SERVER['SCRIPT_NAME'],
-			$_SERVER['REQUEST_URI'],
-			$_SERVER['SERVER_NAME'],
-			$_SERVER['HTTPS'] ?? 'http'
-		)
-	)
-))->render();
+	$source
+) implements Output\Template {
+	private $origin;
+	private $config;
+
+	public function __construct(Output\Template $origin, Ini\Source $config) {
+		$this->origin = $origin;
+		$this->config = $config;
+	}
+
+	public function render(array $variables = []): string {
+		if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+			(new Internal\HeaderExtension($this->config->read()['HEADERS']))->improve();
+			exit;
+		}
+		return $this->origin->render($variables);
+	}
+})->render();
