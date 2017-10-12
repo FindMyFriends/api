516cc4e4128b7ea6c6cd5f9a75ba8c03300e3d29
Dominik Klapuch <klapuchdominik@gmail.com>
2017-10-12 19:46:25 +0200

    Revert "Added Contnet-Range header (#16)"
    This reverts commit e2af6978b97b380dafff81f04e3cbabc1cf350e6.

--- App/V1/Demands/Get.php
+++ App/V1/Demands/Get.php
@@ -27,37 +27,31 @@ final class Get extends V1\Api {
 			);
 			return new Application\RawTemplate(
 				new Response\PaginatedResponse(
-					new Response\ResponseWithRange(
-						new Response\HttpResponse(
-							new Response\JsonResponse(
-								new Response\JsonApiAuthentication(
-									new Response\PlainResponse(
-										new Misc\JsonPrintedObjects(
-											...iterator_to_array(
-												$demands->all(
-													new Dataset\CombinedSelection(
-														new Dataset\SqlRestSort(
-															$_GET['sort'] ?? '',
-															self::ALLOWED_SORTS
-														),
-														new Dataset\SqlPaging(
-															$page,
-															$perPage
-														)
+					new Response\HttpResponse(
+						new Response\JsonResponse(
+							new Response\JsonApiAuthentication(
+								new Response\PlainResponse(
+									new Misc\JsonPrintedObjects(
+										...iterator_to_array(
+											$demands->all(
+												new Dataset\CombinedSelection(
+													new Dataset\SqlRestSort(
+														$_GET['sort'] ?? '',
+														self::ALLOWED_SORTS
+													),
+													new Dataset\SqlPaging(
+														$page,
+														$perPage
 													)
 												)
 											)
 										)
-									),
-									$this->user,
-									$this->url
-								)
+									)
+								),
+								$this->user,
+								$this->url
 							)
-						),
-						'demands',
-						$page,
-						$perPage,
-						$demands->count(new Dataset\EmptySelection())
+						)
 					),
 					$page,
 					new UI\AttainablePagination(
