9e546a07eb16bb9a20a36464a93e39af5d6b3f20
Dominik Klapuch <klapuchdominik@gmail.com>
2017-12-22 20:03:28 +0100

    typo

--- Tests/Postgres/suited_length.sql
+++ Tests/Postgres/suited_length.sql
@@ -18,7 +18,7 @@ END
 $$
 LANGUAGE plpgsql;
 
-CREATE FUNCTION unit_tests.converting_too_big_millimeter_to_centimeter_with_rest() RETURNS TEST_RESULT AS $$
+CREATE FUNCTION unit_tests.converting_too_long_millimeter_to_centimeter_with_rest() RETURNS TEST_RESULT AS $$
 BEGIN
 	RETURN message FROM assert.is_equal(
 		suited_length(ROW(16, 'mm')),
