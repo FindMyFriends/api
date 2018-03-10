bf648139e1c624285f7dd2777dde1ae83a9e1671
Dominik Klapuch <klapuchdominik@gmail.com>
2018-03-10 11:12:51 +0100

    cached configs

--- App/Configuration/ApplicationConfiguration.php
+++ App/Configuration/ApplicationConfiguration.php
@@ -12,21 +12,25 @@ final class ApplicationConfiguration implements Configuration\Source {
 	private const CONFIGURATION = __DIR__ . '/.config.ini',
 		SECRET_CONFIGURATION = __DIR__ . '/.secrets.ini',
 		HASHIDS_CONFIGURATION = __DIR__ . '/.hashids.json',
-		HASHIDS_SECRET_CONFIGURATION = __DIR__ . '/.hashids.secret.json';
+		HASHIDS_SECRET_CONFIGURATION = __DIR__ . '/.hashids.secret.json',
+		CACHE_KEY = 'combined_configs';
 
 	public function read(): array {
-		return (new Configuration\CombinedSource(
-			new Configuration\ValidIni(new \SplFileInfo(self::CONFIGURATION)),
-			new Configuration\ValidIni(new \SplFileInfo(self::SECRET_CONFIGURATION)),
-			new Configuration\NamedSource(
-				'HASHIDS',
-				new CreatedHashids(
-					new Configuration\CombinedSource(
-						new Configuration\ValidJson(new \SplFileInfo(self::HASHIDS_CONFIGURATION)),
-						new Configuration\ValidJson(new \SplFileInfo(self::HASHIDS_SECRET_CONFIGURATION))
+		return (new Configuration\CachedSource(
+			new Configuration\CombinedSource(
+				new Configuration\ValidIni(new \SplFileInfo(self::CONFIGURATION)),
+				new Configuration\ValidIni(new \SplFileInfo(self::SECRET_CONFIGURATION)),
+				new Configuration\NamedSource(
+					'HASHIDS',
+					new CreatedHashids(
+						new Configuration\CombinedSource(
+							new Configuration\ValidJson(new \SplFileInfo(self::HASHIDS_CONFIGURATION)),
+							new Configuration\ValidJson(new \SplFileInfo(self::HASHIDS_SECRET_CONFIGURATION))
+						)
 					)
 				)
-			)
+			),
+			self::CACHE_KEY
 		))->read();
 	}
 }
