722d754972956d5bf68b30703324c941feead24b
Dominik Klapuch <klapuchdominik@gmail.com>
2018-05-14 20:25:32 +0200

    Updated test:

--- Tests/Postgres/demands_trigger_row_bu.sql
+++ Tests/Postgres/demands_trigger_row_bu.sql
@@ -1,22 +1,17 @@
 CREATE FUNCTION unit_tests.throwing_on_changing_created_at() RETURNS test_result
 AS $$
 DECLARE
-  v_freezed_now timestamptz DEFAULT NOW() - INTERVAL '10 MINUTE';
   messages text[];
 BEGIN
   INSERT INTO demands (seeker_id, description_id, created_at, location_id) VALUES (
     (SELECT samples.seeker()),
     (SELECT samples.description()),
-    NOW() - INTERVAL '10 MINUTE',
+    NOW(),
     (SELECT samples.location())
   );
 
   messages = messages || message FROM assert.throws(
     'UPDATE demands SET created_at = NOW()',
-    ROW('Column created_at is read only', 'P0001')::error
-  );
-  messages = messages || message FROM assert.throws(
-    FORMAT ('UPDATE demands SET created_at = %L', v_freezed_now),
     ROW('Column created_at is read only', 'P0001')::error
   );
   RETURN array_to_string(messages, '');
