1cab7b4f32fa4a18f3ecae2805cdaf1c68e2619e
Dominik Klapuch <klapuchdominik@gmail.com>
2018-05-27 22:07:22 +0200

    created base evolution filled all tables

--- Tests/Postgres/created_base_evolution.sql
+++ Tests/Postgres/created_base_evolution.sql
@@ -1,3 +1,26 @@
+CREATE FUNCTION unit_tests.filled_tables() RETURNS test_result
+AS $$
+DECLARE
+  v_seeker_id seekers.id%type;
+  v_evolution_id evolutions.id%type;
+BEGIN
+  SELECT samples.seeker() INTO v_seeker_id;
+  SELECT created_base_evolution(v_seeker_id, 'man'::sex, 1::smallint, '[1996,1996]'::real_birth_year, 'Dom'::text, 'Self'::text) INTO v_evolution_id;
+  RETURN message FROM assert.is_equal(
+    '',
+    (
+      SELECT test_utils.tables_not_matching_count(
+        hstore(
+          ARRAY['hair', 'nails', 'faces', 'teeth', 'hands', 'beards', 'bodies', 'general', 'evolutions', 'descriptions', 'hand_hair', 'seekers', 'eyebrows', 'eyes'],
+          ARRAY[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2]::text[]
+        )
+      )
+    )
+  );
+END
+$$
+LANGUAGE plpgsql;
+
 CREATE FUNCTION unit_tests.throwing_on_created_evolution() RETURNS test_result
 AS $$
 DECLARE
